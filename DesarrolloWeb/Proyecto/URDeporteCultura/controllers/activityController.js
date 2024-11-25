cb3ca294ce02e0767b5ee0e8100e0a1b9417019f
SofiDuartS <sofia.duarte@urosario.edu.co>
2024-11-25 09:04:47 -0500

    Cambiar imagen con multer. Todas las vistas del modulo actividades con alertas de confirmacion

--- DesarrolloWeb/Proyecto/URDeporteCultura/controllers/activityController.js
+++ DesarrolloWeb/Proyecto/URDeporteCultura/controllers/activityController.js
@@ -1,19 +1,20 @@
 import { ActivityModel } from "../models/activityModel.js";
 import fs from 'fs';
 import multer from 'multer';
+import path from 'path';
+
+var upload = multer({
+    dest: 'assets/uploads/',
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+            cb(null, true);
+        } else {
+            return cb({message: 'Ups! Subiste un archivo no permitido. Por favor, selecciona una imagen'}, null);
+        }
+    }
+}).single('imagen');
 
 export const crearActividad = async (req, res) => {
-    var upload = multer({
-        dest: 'assets/uploads/',
-        fileFilter: (req, file, cb) => {
-            if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
-                cb(null, true);
-            } else {
-                return cb({message: 'Ups! Subiste un archivo no permitido. Por favor, selecciona una imagen'}, null);
-            }
-        }
-    }).single('imagen');
-
     upload(req, res, async function(err) {
         if (err) {
             console.log(err.message);
@@ -101,26 +102,54 @@ export const obtenerActividadPorId = async (req, res) => {
 }
 
 export const actualizarActividad = async (req, res) => {
-    try {
-        const dataUpdated = req.body;
+    upload(req, res, async function(err) {
+        if (err) {
+            console.log(err.message);
+            const actividad = await ActivityModel.findById(req.body.idActividad);
+            return res.render('Actividades/editarActividad', {
+                actividad: actividad,
+                mensajeAlerta: err.message,
+            });
+        } else{
+            const actividad = await ActivityModel.findById(req.body.idActividad);
+            if (!req.body.nombre || !req.body.grupo || req.body.grupo === "0" || !req.body.fecha) {
+                const mensajeAlerta = "Por favor, completa todos los campos obligatorios, marcados con *.";
+                return res.render("Actividades/editarActividad", {
+                    actividad: actividad,
+                    mensajeAlerta,
+                });
+            }
+            try {
+                const actividadb = await ActivityModel.findById(req.body.idActividad);
+                const dataUpdated = req.body;
+                if (req.file) {
+                    console.log("Nueva imagen subida:", req.file);
+                    const newImagePath = saveImage(req.file);
+                    if (actividadb.imagen) {
+                        deleteImage(actividadb.imagen); // Eliminar la imagen existente
+                    }
+                    dataUpdated.imagen = newImagePath;
+                }
+                await ActivityModel.updateOne({_id: req.body.idActividad}, {
+                    nombre: dataUpdated.nombre, 
+                    grupo: dataUpdated.grupo, 
+                    fecha: dataUpdated.fecha, 
+                    ubicacion: dataUpdated.ubicacion,
+                    resultado: dataUpdated.resultado,
+                    imagen: dataUpdated.imagen || actividadb.imagen,
+                });
+                const actividad = await ActivityModel.findById(req.body.idActividad);
+                res.render('Actividades/consultarActividadParticular', {actividad: actividad});
+                // res.status(200).json(actividades);
+                console.log("Actividad actualizada correctamente");
         
-        await ActivityModel.updateOne({_id: req.body.idActividad}, {
-            nombre: dataUpdated.nombre, 
-            grupo: dataUpdated.grupo, 
-            fecha: dataUpdated.fecha, 
-            ubicacion: dataUpdated.ubicacion,
-            resultado: dataUpdated.resultado,
-            imagen: dataUpdated.imagen
-        });
-        const actividad = await ActivityModel.findById(req.body.idActividad);
-        res.render('Actividades/consultarActividadParticular', {actividad: actividad});
-        // res.status(200).json(actividades);
-        console.log("Actividad actualizada correctamente");
-
-    } catch (error) {
-        // res.status(400).json({mensaje: error.message});
-        console.log("Error al actualizar la actividad");
-    }
+            } catch (error) {
+                // res.status(400).json({mensaje: error.message});
+                console.log(error.message);
+            }
+            
+        }
+    });
 }
 
 // Cambiando eliminar por inhabilitar
@@ -157,7 +186,7 @@ export const formularioActualizarActividad = async (req, res) => {
     try {
         const id = req.params.id;
         const actividad = await ActivityModel.findById(id);
-        res.render('Actividades/editarActividad', {actividad: actividad});
+        res.render('Actividades/editarActividad', {actividad: actividad, mensajeAlerta: null});
     } catch (error) {
         // res.status(400).json({mensaje: error.message});
         console.log(error);
@@ -166,13 +195,22 @@ export const formularioActualizarActividad = async (req, res) => {
 
 // Subir imagen usando multer
 function saveImage(file) {
-    let newPath = `assets/uploads/${file.originalname}`;
+    const date = Date.now();
+    const ext = path.extname(file.originalname);
+    let newPath = `assets/uploads/${date}${ext}`;
     console.log(newPath);
     console.log(file.path);
     fs.renameSync(file.path, newPath);
-    newPath = `/uploads/${file.originalname}`;
+    newPath = `/uploads/${date}${ext}`;
     return newPath;
 }
+
+const deleteImage = (imagePath) => {
+    const pathImage = `assets${imagePath}`;
+    if (fs.existsSync(pathImage)) {
+        fs.unlinkSync(pathImage); // Eliminar el archivo del sistema
+    }
+};
 
 export const subirImagen = (req, res) => {
     var upload = multer({
